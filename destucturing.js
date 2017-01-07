a0dedb1200a1b13e15595215944cfa633b8398ed
tranquilLake <mycomesh@163.com>
2017-01-07 20:54:48 +0800

    object destructing

--- destucturing.js
+++ destucturing.js
@@ -57,3 +57,63 @@ function* fibs() {
 let [fib_0, fib_1, fib_2, fib_3, fib_4, fib_5, fib_6] = fibs();
 console.log(fib_0, fib_1, fib_2,                // print 0 1 1 2 3 5 8
     fib_3, fib_4, fib_5, fib_6);
+
+// default destructing value
+var [foo_9 = true] = [];
+console.log(foo_9);                             // print true, because there is a default value
+
+let [x_10, y_10 = "b"] = ["a"];
+let [x_11, y_11 = "b"] = ["a", undefined];
+let [x_12, y_12 = "b"] = ["a", null];
+console.log(x_10, y_10);                        // print a b
+console.log(x_11, y_11);                        // print a b, because an undefined value can trigger the default value
+console.log(x_12, y_12);                        // print a null, because a null value is not an undefined value, and can not trigger the default value
+
+let getA = () => {
+    console.log("getA...");
+    return "a";
+};
+let getB = () => {
+    console.log("getB...");
+    return "b";
+};
+
+let [a_13 = getA()] = ["a"];                    // print nothing
+let [b_13 = getB()] = [];                       // print getB..., because the getB() function will be invoked while the default value is triggered.
+
+// object destructing
+let {foo_14, bar_14} = { foo_14: "a", bar_14: "b" };
+console.log(foo_14, bar_14);                    // print a b
+
+// object destructing and map to different name
+let {foo_15: foo_16, bar_15: bar_16} = { foo_15: "a", bar_15: "b" };
+console.log(foo_16, bar_16);                    // print a b
+
+// object destructing nest
+let obj_16 = {
+    a: "a",
+    b: [
+        "c",
+        {
+            d: "d"
+        }
+    ]
+}
+
+let {b: [c_16, { d: d_16 }]} = obj_16;
+console.log(d_16);                              // print d_16
+
+let obj_17 = {
+    a: {
+        b: {
+            c: {
+                d: {
+                    e: "e"
+                }
+            }
+        }
+    }
+}
+
+let {a: {b: {c: {d: {e: e_17}}}}} = obj_17;
+console.log(e_17);                              // print e, and a, b, c, e are all pattern but not variable define
