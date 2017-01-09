af18a854ee49b08533a1b5faa520e0bd35e97676
wangminjun <wangminjun@juxinli.com>
2017-01-09 17:55:07 +0800

    string normalize function

--- string_demo.js
+++ string_demo.js
@@ -55,3 +55,11 @@ console.log("𠮷".charAt(0));            // can not print 𠮷
 
 // string.at function come with es6 can handle 32-bit charater
 // console.log("𠮷".at(0));
+
+// nomalize function
+console.log("\u01D1");                  // print Ǒ
+console.log("\u004F\u030C");            // print Ǒ
+console.log(                            // print false, because them have same meaning but not same character
+    "\u01D1" === "\u004F\u030C");   
+console.log(                            // print true, because "\u004F\u030C" has bean changed to "\u01D1"
+    "\u01D1" === "\u004F\u030C".normalize());
