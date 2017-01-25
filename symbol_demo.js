4285e2a722436a83296adfa4d2b34cef6b1c071e
wangminjun <wangminjun@juxinli.com>
2017-01-25 16:02:23 +0800

    angular hello world

--- symbol_demo.js
+++ symbol_demo.js
@@ -32,3 +32,14 @@ let sym_8 = Symbol("bar");
 
 console.log(sym_7);                                         // print Symbol(foo);
 console.log(sym_8);                                         // print Symbol(bar);
+
+// if give a none string parameter to built-in function Symbol, Symbol will get string infomation of the object by using the toString function
+
+let obj_9 = {
+    toString: function () {
+        return "hello";
+    }
+}
+
+let sym_10 = Symbol(obj_9);
+console.log(sym_10);                                        // print Symbol(hello);
